f7535938c7b8c7ff40bdd640e4f27c7b2b74c23f
jayden-rohwein <jayrohwein@yahoo.com>
2019-04-17 18:30:32 -0400

    initial setup

--- src/index.js
+++ src/index.js
@@ -1,24 +1,5 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import "./index.css";
-import App from "./App";
-import * as serviceWorker from "./serviceWorker";
-import "bootstrap/dist/css/bootstrap.min.css";
 
-import AdminLayout from "./components/admin/layouts/adminLayout/adminLayout";
-import Firebase, { FirebaseContext } from "./components/Firebase";
-
-const getFirebase = () => {
-  return new Firebase();
-};
-ReactDOM.render(
-  <FirebaseContext.Provider value={getFirebase()}>
-    <App />
-  </FirebaseContext.Provider>,
-  document.getElementById("root")
-);
-
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: http://bit.ly/CRA-PWA
-serviceWorker.unregister();
+import App from "./app";
+ReactDOM.render(<App />, document.getElementById("app"));
