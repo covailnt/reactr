12b29eff2557092d6a7a754ca88f894f14c35212
jayden-rohwein <jayrohwein@yahoo.com>
2019-05-07 18:29:49 -0400

    comment out middleware error
    something in our middleware was breaking so I commented it out

--- src/store/index.js
+++ src/store/index.js
@@ -2,10 +2,10 @@ import { createStore, applyMiddleware } from "redux";
 import rootReducer from "./reducers";
 import thunk from "redux-thunk";
 
-const middleware = applyMiddleware([
-  thunk,
-  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
-]);
-const store = createStore(rootReducer, middleware);
+//const middleware = applyMiddleware([
+// thunk,
+//  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+//]);`
+const store = createStore(rootReducer, {}); // middleware);
 
 export default store;
