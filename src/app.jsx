00bb232a586d702ab7b0b1d9dbe2e85113a64cf1
jayden-rohwein <jayrohwein@yahoo.com>
2019-05-07 19:38:37 -0400

    how much wood would a wood chuck chuck

--- src/app.jsx
+++ src/app.jsx
@@ -9,6 +9,15 @@ import { Provider } from "react-redux";
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import { syncHistoryWithStore } from "react-router-redux";
 
+// import Firebase, { FirebaseContext } from "./components/Firebase";
+
+// ReactDOM.render(
+//   <FirebaseContext.Provider value={new Firebase()}>
+//     <App />
+//   </FirebaseContext.Provider>,
+//   document.getElementById("root")
+// );
+
 class App extends Component {
   render() {
     return (
